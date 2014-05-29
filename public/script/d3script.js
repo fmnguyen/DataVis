171b1c8af1498037bb76d41a7cab61456e098307
fmnguyen <fmnguyen@uw.edu>
2014-05-29 16:57:43 -0700

    Added start and stop bash scripts

--- public/script/d3script.js
+++ public/script/d3script.js
@@ -10,7 +10,7 @@ window.onload = function() {
 	var bar = chart.selectAll("div");
 	var barUpdate = bar.data(data);
 	var barEnter = barUpdate.enter().append("div");
-	barEnter.style("width", function(d) { return d * 10 + "px"; });
+	barEnter.style("width", function(d) { return x(d) + "px"; });
 	barEnter.text(function(d) { return d; });
 
 	/*
