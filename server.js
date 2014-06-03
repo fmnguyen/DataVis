5b15f14edfb1b6e40cf69b3797235a233ca9cbdc
fmnguyen <fmnguyen@uw.edu>
2014-06-03 14:54:06 -0700

    Changed port back to 8080

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // own modules
-var port = 1234;
+var port = 8080;
 var FileServer = require('./module/fileServer');
 var fileServer = new FileServer(port);
 console.log("Server now running on port " + port);
