fec502484ee8454d49e8fbe55feccad19cddda05
fmnguyen <fmnguyen@uw.edu>
2014-06-03 14:50:17 -0700

    Added server port

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
 // own modules
+var port = 1234;
 var FileServer = require('./module/fileServer');
-var fileServer = new FileServer(8080);
+var fileServer = new FileServer(port);
+console.log("Server now running on port " + port);
