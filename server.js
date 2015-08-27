56e02d174d735f534985b53dba8dba16cdb0f561
fmnguyen <fmnguyen@uw.edu>
2015-08-27 12:03:28 -0700

    Preparing for deployment to heroku

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // own modules
-var port = 8080;
+var port = 5000;
 var FileServer = require('./module/fileServer');
 var fileServer = new FileServer(port);
 console.log("Server now running on port " + port);
